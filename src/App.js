e9398fbf8554f164d545937c102db08bbbc7fabf
Priya Jeg <priya.sashi09@gmail.com>
2023-08-31 18:21:18 -0400

    make change to home page

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
-const Image = styled.img`height:100vh;width:100%;`;
 const App = () => {
   return (
     <Container>
@@ -31,8 +30,6 @@ const App = () => {
           <Route path="/contact" element={<Contact />}></Route>
         </Routes>
         <Texture />
-
-        <Image src="https://images.unsplash.com/photo-1499951360447-b19be8fe80f5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" />
 
         <Footer />
       </Router>
